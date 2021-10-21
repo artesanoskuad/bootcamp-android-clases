fda1c47b07043172f0699085a72ce91ad519df11
hardroid98 <artesano.skuad@gmail.com>
2021-10-20 21:14:18 -0300

    Se agrega Navigation

--- trivias-fragment/app/src/main/java/com/artesanoskuad/trivia/TriviaFragment.java
+++ trivias-fragment/app/src/main/java/com/artesanoskuad/trivia/TriviaFragment.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.artesanoskuad.trivia.databinding.FragmentTriviaBinding;
@@ -28,6 +31,16 @@ public class TriviaFragment extends Fragment {
         initBinding(inflater, container);
 
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Bundle arguments = getArguments();
+        if(arguments != null){
+            String nombre = arguments.getString("name");
+            Toast.makeText(getContext(), "Nombre: " + nombre, Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void initBinding(LayoutInflater inflater, ViewGroup container) {
--- trivias-fragment/app/src/main/java/com/artesanoskuad/trivia/WelcomeFragment.java
+++ trivias-fragment/app/src/main/java/com/artesanoskuad/trivia/WelcomeFragment.java
@@ -1,35 +1,27 @@
 package com.artesanoskuad.trivia;
 
+import static com.artesanoskuad.trivia.WelcomeFragmentDirections.*;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.Navigation;
 
 import com.artesanoskuad.trivia.databinding.FragmentWelcomeBinding;
 
 public class WelcomeFragment extends Fragment {
 
+    private static final String EMPTY_USER_ERROR = "Ingrese nombre";
     private FragmentWelcomeBinding binding;
-    private TriviaFragment triviaFragment;
 
     public WelcomeFragment() {
         // Required empty public constructor
-    }
-
-
-    public static WelcomeFragment newInstance() {
-        WelcomeFragment fragment = new WelcomeFragment();
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
     }
 
     @Override
@@ -46,22 +38,27 @@ public class WelcomeFragment extends Fragment {
 
     private void setupOnClickListener() {
         binding.btnComenzar.setOnClickListener(v -> {
-            goToTriviaFragment();
+            if(binding.etNombre.getText().toString().isEmpty()){
+                showEmptyUserMessage();
+            } else {
+                goToTriviaFragment();
+            }
         });
     }
 
-    private void goToTriviaFragment() {
-        //todo: acá validar que el edit text tenga un nombre de usuario.
-
-        if (triviaFragment == null) {
-            triviaFragment = TriviaFragment.newInstance();
-        }
-
-        FragmentManager fragmentManager = getParentFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.content, triviaFragment)
-                .addToBackStack(null)
-                .commit();
-
+    private void showEmptyUserMessage() {
+        Toast.makeText(getContext(), EMPTY_USER_ERROR, Toast.LENGTH_SHORT).show();
+        binding.etNombre.setError(EMPTY_USER_ERROR);
+        binding.etNombre.requestFocus();
     }
+
+    private void goToTriviaFragment() {
+        String nombre = binding.etNombre.getText().toString();
+        //ActionWelcomeFragmentToTriviaFragment action = actionWelcomeFragmentToTriviaFragment(nombre);
+        //Navigation.findNavController(binding.getRoot()).navigate(action);
+        Bundle bundle = new Bundle();
+        bundle.putString("name", nombre);
+        Navigation.findNavController(binding.getRoot()).navigate(R.id.action_welcomeFragment_to_triviaFragment, bundle);
+    }
+
 }
