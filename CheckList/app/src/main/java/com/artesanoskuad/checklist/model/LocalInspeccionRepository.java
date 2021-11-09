0f8d7429b33a5e479f6950bafbebb7fe4d40f0b4
hardroid98 <artesano.skuad@gmail.com>
2021-11-08 21:32:43 -0300

    Se crean inspecciones fake para revisar HashMap

--- CheckList/app/src/main/java/com/artesanoskuad/checklist/model/LocalInspeccionRepository.java
+++ CheckList/app/src/main/java/com/artesanoskuad/checklist/model/LocalInspeccionRepository.java
@@ -6,15 +6,32 @@ import java.util.List;
 
 public class LocalInspeccionRepository implements  InspeccionRepository {
 
-    private List<Inspeccion> inspeccionesEnMemoria;
+    private final List<Inspeccion> inspeccionesEnMemoria;
 
     public LocalInspeccionRepository() {
         inspeccionesEnMemoria = new ArrayList<>();
         inspeccionesEnMemoria.add(new Inspeccion(
-                new InspeccionDetalle("Acer", "Acer2021", "08-11-2021"),
-                new HashMap<>(),
+                crearDetalleInspeccion("Acer", "Acer2021", "08-11-2021"),
+                crearCheckListInspeccion(),
                 "Obs 1234"
         ));
+    }
+
+    private HashMap<String, Boolean> crearCheckListInspeccion() {
+        HashMap<String, Boolean> checkList = new HashMap<>();
+        checkList.put("Equipo en caja", true);
+        checkList.put("Cargador en caja", false);
+        checkList.put("Manual en caja", true);
+        checkList.put("Garantía en caja", true);
+        checkList.put("Equipo enciende carga SO", true);
+        checkList.put("Monitor sin problemas", true);
+        checkList.put("Audio sin problemas", true);
+        checkList.put("Touchpad sin problemas", true);
+        return checkList;
+    }
+
+    private InspeccionDetalle crearDetalleInspeccion(String marca, String modelo, String fecha) {
+        return new InspeccionDetalle(marca, modelo, fecha);
     }
 
     @Override
